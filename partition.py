085dff0616d5ce954d60fda784eec94a7a22521d
lukebrichey <lukebrichey@gmail.com>
2023-04-22 11:01:31 -0400

    Check?

--- partition.py
+++ partition.py
@@ -1,68 +1,46 @@
 import random
 import sys
-import math 
+import math
 
 max_iter = 25000
 
 def pp(nums, P):
     n = len(nums)
-    prepartitions = {i: [] for i in range(1, n + 1)}
+    prepartitions = {i: 0 for i in range(1, n + 1)}
     for i in range(n):
-        prepartitions[P[i]].append(nums[i])
+        prepartitions[P[i]] += nums[i]
 
-    s1 = []
-    s2 = []
-    for i, subset in prepartitions.items():
+    s1_sum = 0
+    s2_sum = 0
+    for i, value in prepartitions.items():
         if i % 2 == 0:
-            s1.extend(subset)
+            s1_sum += value
         else:
-            s2.extend(subset)
+            s2_sum += value
 
-    return s1, s2
-
+    return abs(s1_sum - s2_sum)
 
 def Karmarkar_Karp(nums):
-    s1 = nums 
-    s2 = []
+    return pp(nums, list(range(1, len(nums) + 1)))
 
-    # Keep subtracting the largest number from the second largest number.
-    while len(s1) > 1:
-        s1.sort(reverse=True)
-        s2.append(s1.pop(0) - s1.pop(0))
-    
-    # Return the difference between the two sets.
-    return abs(s1[0] - sum(s2))
-
-
-# Generate a random solution
 def rand_sol(n):
     return [random.randint(1, n) for _ in range(n)]
 
-
 def RepeatedRandom(nums, prepartition):
     n = len(nums)
-    if prepartition:
-        P = rand_sol(n)
-        s1, s2 = pp(nums, P)
-    else:
-        s1 = nums
-        s2 = []
-
-    best_diff = abs(sum(s1) - sum(s2))
+    P = rand_sol(n)
+    best_diff = pp(nums, P) if prepartition else Karmarkar_Karp(nums)
 
     for _ in range(max_iter):
         new_P = rand_sol(n)
-        temp_s1, temp_s2 = pp(nums, new_P) if prepartition else (new_P, [])
-        temp_diff = abs(sum(temp_s1) - sum(temp_s2))
+        temp_diff = pp(nums, new_P) if prepartition else Karmarkar_Karp(new_P)
 
         if temp_diff < best_diff:
             best_diff = temp_diff
-            s1, s2 = temp_s1, temp_s2
+            P = new_P
 
     return best_diff
 
-
-# Create a random neighbor
 def random_neighbor(P):
     n = len(P)
     i = random.randint(0, n - 1)
@@ -73,69 +51,43 @@ def random_neighbor(P):
     new_P[i] = j
     return new_P
 
-
 def HillClimbing(nums, prepartition):
     n = len(nums)
-    if prepartition:
-        P = rand_sol(n)
-        s1, s2 = pp(nums, P)
-    else:
-        s1 = nums
-        s2 = []
-
-    best_diff = abs(sum(s1) - sum(s2))
+    P = rand_sol(n)
+    best_diff = pp(nums, P) if prepartition else Karmarkar_Karp(nums)
 
     for _ in range(max_iter):
         new_P = random_neighbor(P)
-        temp_s1, temp_s2 = pp(nums, new_P) if prepartition else (new_P, [])
-        temp_diff = abs(sum(temp_s1) - sum(temp_s2))
+        temp_diff = pp(nums, new_P) if prepartition else Karmarkar_Karp(new_P)
 
         if temp_diff < best_diff:
             best_diff = temp_diff
-            s1, s2 = temp_s1, temp_s2
             P = new_P
 
     return best_diff
 
-
-# Cooling schedule
 def T(iter):
     return 10**10 * (0.8)**(iter // 300)
 
-
 def SimulatedAnnealing(nums, prepartition):
     n = len(nums)
-    if prepartition:
-        P = rand_sol(n)
-        s1, s2 = pp(nums, P)
-    else:
-        s1 = nums
-        s2 = []
-
-    best_diff = abs(sum(s1) - sum(s2))
+    P = rand_sol(n)
+    best_diff = pp(nums, P) if prepartition else Karmarkar_Karp(nums)
     best_P = P
 
     for iter in range(max_iter):
         new_P = random_neighbor(P)
-        temp_s1, temp_s2 = pp(nums, new_P) if prepartition else (new_P, [])
-        temp_diff = abs(sum(temp_s1) - sum(temp_s2))
+        temp_diff = pp(nums, new_P) if prepartition else Karmarkar_Karp(new_P)
 
         if temp_diff < best_diff:
             best_diff = temp_diff
             best_P = P
 
-        delta = temp_diff - abs(sum(s1) - sum(s2))
+        delta = temp_diff - (pp(nums, P) if prepartition else Karmarkar_Karp(nums))
         if delta < 0 or random.random() < math.exp(-delta / T(iter)):
-            s1, s2 = temp_s1, temp_s2
             P = new_P
 
-    if prepartition:
-        s1, s2 = pp(nums, best_P)
-    else:
-        s1, s2 = best_P, []
-
-    return abs(sum(s1) - sum(s2))
-
+    return best_diff
 
 """
 CODE FOR EXPERIMENTS i.e. FLAG 1
@@ -173,19 +125,16 @@ def calculate_average_residues(results):
         average_residues[algo_code] = sum(residues) / len(residues)
     return average_residues
 
-
 def main():
     # Usage: python partition.py <flag> <algorithm> <input file>
 
-    # Get the algorithm and input file 
     flag = int(sys.argv[1])
     algo = int(sys.argv[2])
 
     if flag == 0:
         with open(sys.argv[3], 'r') as f:
-            nums = [int(line) for line in f]    
+            nums = [int(line) for line in f]
 
-        # Choose algorithm, args: <nums>, <prepartition>
         if algo == 0:
             print(Karmarkar_Karp(nums))
         elif algo == 1:
@@ -202,13 +151,18 @@ def main():
             print(SimulatedAnnealing(nums, True))
 
     elif flag == 1:
-        instances = generate_instances()
+        # Run experiments
+        instances = []
+        for _ in range(50):
+            instance = [random.randint(1, 10**12) for _ in range(100)]
+            instances.append(instance)
+
         results = run_experiments(instances)
         average_residues = calculate_average_residues(results)
         print("Average residues:")
         for algo_code, avg_residue in average_residues.items():
             print(f"Algorithm {algo_code}: {avg_residue}")
 
-    
+
 if __name__ == '__main__':
     main()
